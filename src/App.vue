dac5b3677a47af2965ed3625894a841e66dd5943
Greta Kavaliauskaite <kavaliauskaite3@gmail.com>
2021-04-30 15:36:25 +0100

    initail commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/bomba.jpeg" width="100%" height="auto" style="max-width: 450px;">
+    <Home msg="Jau greitai..."/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
